6c381d9aaa37f4a53eb9b0c07471e476a384200a
OrangeM21 <orangemtony@gmail.com>
2019-01-20 23:42:10 +0800

    fix

--- js2/js/player.js
+++ js2/js/player.js
@@ -1,3 +1,97 @@
-$(function(){
-    
+$(function () {
+    function setDefault() {
+        $('#inputnumber').val(4);
+        $('#customRange2').val(4);
+        changedisplay(4);
+    }
+    function checkValue() {
+        var number = Number($('#inputnumber').val());
+        if (Number.isInteger(number) && number >= 4 && number <= 18) {
+            return true;
+        } else {
+            $('#exampleModal').modal();
+            setDefault();
+            return false;
+        }
+    }
+    // 4 ---- 1
+    // 5 ---- 1
+    // 6 ---- 2
+    // 7 ---- 2
+    // 8 ---- 2
+    // 9 ---- 3
+    // 10 ---- 3
+    // 11 ---- 3
+    // 12 ---- 4
+    // 13 ---- 4
+    // 14 ---- 4
+    // 15 ---- 4
+    // 16 ---- 5
+    // 17 ---- 5
+    // 18 ---- 5
+    const ruler = ["#ruler1", "#ruler2", "#ruler3", "#ruler4", "#ruler5"];
+    const people = ["#people1", "#people2", "#people3", "#people4", "#people5", "#people6", "#people7", "#people8", "#people9", "#people10", "#people11", "#people12", "#people13"];
+
+    function reedomNumber() {
+        return ("-" + Math.floor((Math.random() * (18 - 4) + 4)));
+    }
+
+    function setnormal(){
+        for (let i = 0; i < ruler.length; i++) {
+            $(ruler[i]).removeClass("visible").addClass("unvisible").css("order", "0");
+        }
+        for (let i = 0; i < people.length; i++) {
+            $(people[i]).removeClass("visible").addClass("unvisible").css("order", "0");
+        }
+    }
+
+    function changedisplay(n) {
+        setnormal();
+        let rulernumber;
+        if (n >= 4 && n <= 5) {
+            rulernumber = 1;
+        }else if(n >= 6 && n <= 8){
+            rulernumber = 2;
+        }else if(n >= 9 && n <= 11){
+            rulernumber = 3;
+        }else if(n >= 12 && n <= 15){
+            rulernumber = 4;
+        }else{
+            rulernumber = 5;
+        }
+
+        let peoplenumber = n - rulernumber;
+
+        for (let i = 0; i < rulernumber; i++) {
+            $(ruler[i]).removeClass("unvisible").addClass("visible").css("order", "-2" + i);
+        }
+        for (let i = 0; i < peoplenumber; i++) {
+            $(people[i]).removeClass("unvisible").addClass("visible").css("order", "-1" + i);
+        }
+
+    }
+
+    function reedom() {
+        $(".visible").each(function () {
+            $(this).css("order", reedomNumber());
+        });
+    }
+
+    $('#customRange2').bind('input', function () {
+        $("#inputnumber").val($(this).val());
+        changedisplay($(this).val());
+    })
+
+    $("#inputnumber").change(function (e) {
+        if (checkValue()) {
+            $('#customRange2').val($(this).val());
+            console.log($(this).val());
+            changedisplay(Number($('#inputnumber').val()));
+        }
+    });
+
+    $('#ok').click(function () {
+        reedom();
+    });
 })
+
