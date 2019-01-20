8c9a9968401f02b6806bc5decb044788fa8a3d70
OrangeM21 <orangemtony@gmail.com>
2019-01-20 14:23:41 +0800

    fix

--- js1/index.js
+++ js1/index.js
@@ -2,37 +2,48 @@
 var startbtn = document.getElementById("start");
 var endbtn = document.getElementById("stop");
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var colors = [];
 var id = 0;
+var havetimer = false;
 
 var getRandomColor = function () {
     return '#' +
         (function (color) {
             return (color += '0123456789abcdef'[Math.floor(Math.random() * 16)])
-                && (color.length == 6) ? color : arguments.callee(color);
+                && (color.length == 6) && (color != 'f8a501') ? color : arguments.callee(color);
         })('');
 }
 
 var reedom = function () {
-    id = setInterval(
-        function () {
-            for (let t = 0; t < arr.length; t++) {
-                let element = document.getElementById(arr[t]);
-                element.style.background = 'orange';
-            }
+    if (!havetimer) {
+        havetimer = true;
+        id = setInterval(
+            function () {
+                for (let t = 0; t < arr.length; t++) {
+                    let element = document.getElementById(arr[t]);
+                    element.style.background = 'orange';
+                }
 
-            var result = arr.sort(function () { return 0.5 - Math.random(); }).slice(0, 3);
+                var result = arr.sort(function () { return 0.5 - Math.random(); }).slice(0, 3);
 
-            for (let i = 0; i < result.length; i++) {
-                let element = document.getElementById(result[i]);
-                element.style.background = getRandomColor();
-            }
-        },
-        3000
-    );
+                for (let i = 0; i < result.length; i++) {
+                    colors[i] = getRandomColor();
+                    if (colors[i] == colors[i - 1]) {
+                        colors[i] = getRandomColor();
+                    }
+                    let element = document.getElementById(result[i]);
+                    element.style.background = colors[i];
+                }
+            },
+            3000
+        );
+    }
+
 }
 
 var clearReedom = function () {
     clearInterval(id);
+    havetimer = false;
     for (let t = 0; t < arr.length; t++) {
         let element = document.getElementById(arr[t]);
         element.style.background = 'orange';
