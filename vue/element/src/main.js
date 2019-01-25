4b921b03380cc1bd26c9933091ed26c0348b9ebd
OrangeM21 <orangemtony@gmail.com>
2019-01-25 11:49:49 +0800

    fiｘ

--- vue/element/src/main.js
+++ vue/element/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/app.vue'
 import './plugins/element.js'
+import store from './store/Store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
